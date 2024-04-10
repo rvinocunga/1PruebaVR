21650be7166f51a499e934303eff303b726e9214
rvinocunga <adam22rvinocunga@inslaferreria.cat>
2024-04-10 19:57:10 +0200

    Script de diana
    El script funcional, añade la puntuación al HUD y elimina la DIANA.
    Se puede poner el valor que quieras con el script, se puede poner el mismo script a diferentes objetivos tal como botellas, dianas pequeñas, etc...

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int PuntosTotales { get { return puntosTotales; } }
+    private int puntosTotales;
+
+    public void SumarPuntos(int puntosASumar)
+    {
+        puntosTotales += puntosASumar;
+    }
+}
--- Assets/Scripts/ObjetivoDiana.cs
+++ Assets/Scripts/ObjetivoDiana.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class ObjetivoDiana : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public int valor;
+    public GameManager gameManager;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if(collision.gameObject.CompareTag("Misil"))
+        {
+            gameManager.SumarPuntos(valor);
+            Destroy(this.gameObject);
+        }
     }
 }
